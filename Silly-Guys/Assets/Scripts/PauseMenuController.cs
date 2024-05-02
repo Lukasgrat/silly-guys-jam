2f3e9440467d822b5a90f7f12686865350939c09
Tomahawk2191 <polite.i@northeastern.edu>
2024-05-02 19:17:21 -0400

    added pause to second level

--- Silly-Guys/Assets/Scripts/PauseMenuController.cs
+++ Silly-Guys/Assets/Scripts/PauseMenuController.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 
-public class MenuController : MonoBehaviour
+public class PauseMenuController : MonoBehaviour
 {
-    [SerializeField] GameObject canvas;
+    [SerializeField] GameObject PauseMenu;
 
     private void Update()
     {
         // should change over to pause menu, but isn't
         // debug log shows that this runs but canvas won't change
-        if (Input.GetKeyDown("escape") && !canvas.activeInHierarchy)
+        if (Input.GetKeyDown("escape") && !PauseMenu.activeInHierarchy)
         {
             Time.timeScale = 0;
             gameObject.GetComponent<PlayerController>().isLocked = true;
-            canvas.SetActive(true);
+            PauseMenu.SetActive(true);
         }
 
         //if (Input.GetKeyDown("escape") && canvas.activeInHierarchy)
