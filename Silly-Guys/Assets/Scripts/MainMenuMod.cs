25008ae6e063f74f35cc5ed1b5749faf5126f324
Tomahawk2191 <polite.i@northeastern.edu>
2024-05-02 16:09:45 -0400

    pause menu done for level 1

--- Silly-Guys/Assets/Scripts/MainMenuMod.cs
+++ Silly-Guys/Assets/Scripts/MainMenuMod.cs
@@ -3,13 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuMod : MonoBehaviour
 {
-    [SerializeField]
-    GameObject canvas;
-    [SerializeField]
-    GameObject player;
+    [SerializeField] GameObject canvas;
+    [SerializeField] GameObject player;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            playGame();
+        }
+    }
     public void playGame()
     {
+        Time.timeScale = 1;
         player.GetComponent<PlayerController>().isLocked = false;
         canvas.SetActive(false);
     }
--- Silly-Guys/Assets/Scripts/MenuController.cs
+++ Silly-Guys/Assets/Scripts/MenuController.cs
@@ -2,28 +2,24 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-    [SerializeField]
-    bool active = false;
-    [SerializeField]
-    GameObject canvas;
+    [SerializeField] GameObject canvas;
 
     private void Update()
     {
         // should change over to pause menu, but isn't
         // debug log shows that this runs but canvas won't change
-        if (Input.GetKeyDown("escape") && !active)
+        if (Input.GetKeyDown("escape") && !canvas.activeInHierarchy)
         {
+            Time.timeScale = 0;
             gameObject.GetComponent<PlayerController>().isLocked = true;
-            Debug.Log("esc");
             canvas.SetActive(true);
-            active = true;
         }
 
-        if (Input.GetKeyDown("escape") && canvas.activeInHierarchy)
-        {
-            canvas.SetActive(false);
-            active = false;
-            gameObject.GetComponent<PlayerController>().isLocked = false;
-        }
+        //if (Input.GetKeyDown("escape") && canvas.activeInHierarchy)
+        //{
+        //    Time.timeScale = 0;
+        //    canvas.SetActive(true);
+        //    gameObject.GetComponent<PlayerController>().isLocked = false;
+        //}
     }
 }
